8e071eefedbbe3be8b70dd131fb2ed377df0bfae
ThanhLOL123 <104844391+ThanhLOL123@users.noreply.github.com>
2024-11-30 22:20:00 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import streamlit as st
 st.markdown("""
     <style>
     .stApp {
-        background-image: url('https://your-image-url.jpg');
+        background-image: url('https://www.google.com/imgres?q=background%20&imgurl=https%3A%2F%2Fpng.pngtree.com%2Fthumb_back%2Ffh260%2Fbackground%2F20230408%2Fpngtree-rainbow-curves-abstract-colorful-background-image_2164067.jpg&imgrefurl=https%3A%2F%2Fwww.hotelmariahilf.at%2F%3Fh%3D136181815&docid=p41CiuxunHaUHM&tbnid=Y7_wpVjsIJvqbM&vet=12ahUKEwj296KUrYSKAxWkgK8BHYvrG7IQM3oECBYQAA..i&w=641&h=360&hcb=2&itg=1&ved=2ahUKEwj296KUrYSKAxWkgK8BHYvrG7IQM3oECBYQAA');
         background-size: cover;
         background-position: center;
         height: 100vh;
